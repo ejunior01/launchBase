eb2c99f9ef5e1de6733df5e96b35a4f6a1c6c350
Edvan Ribeiro <jk.jr47@gmail.com>
2020-07-23 20:41:52 -0300

    inclusão novo desafio

---
+++ desafios/desafio_03/desafio_3_2/public/js/scripts.js
@@ -0,0 +1,30 @@
+
+const modal = document.querySelector(".modal-overlay")
+const iframe = document.querySelector(".modal-iframe iframe")
+const cards = document.querySelectorAll(".card-courses")
+
+for ( let card of cards) {
+    card.querySelector(".button").addEventListener('click', ()=> {
+
+        modal.classList.add('active')
+
+        if (modal.querySelector(".modal").classList.contains('maximize')) {
+            modal.querySelector(".modal").classList.remove('maximize')
+        }
+       
+        let idCourser = card.getAttribute("id")
+
+        iframe.setAttribute('src', `https://rocketseat.com.br/${idCourser}`)
+
+    })
+}
+
+modal.querySelector(".modal .button-close").addEventListener('click', ()=> {
+    modal.classList.remove('active')
+    iframe.setAttribute('src','')
+})
+
+modal.querySelector(".modal .button-maximize").addEventListener('click', ()=> {
+    modal.querySelector(".modal").classList.toggle('maximize')
+})
+
