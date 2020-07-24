07836aaed0097c3d23c306df38bb5731a70df445
Edvan Ribeiro <jk.jr47@gmail.com>
2020-07-23 21:35:07 -0300

    inclusão de novo desafio

--- desafios/desafio_03/desafio_3_2/public/js/scripts.js
+++ desafios/desafio_03/desafio_3_2/public/js/scripts.js
@@ -1,30 +1,26 @@
+const modal = document.querySelector(".modal-overlay");
+const iframe = document.querySelector(".modal-iframe iframe");
+const cards = document.querySelectorAll(".card-courses");
 
-const modal = document.querySelector(".modal-overlay")
-const iframe = document.querySelector(".modal-iframe iframe")
-const cards = document.querySelectorAll(".card-courses")
+for (let card of cards) {
+  card.querySelector(".button").addEventListener("click", () => {
+    modal.classList.add("active");
 
-for ( let card of cards) {
-    card.querySelector(".button").addEventListener('click', ()=> {
+    if (modal.querySelector(".modal").classList.contains("maximize")) {
+      modal.querySelector(".modal").classList.remove("maximize");
+    }
 
-        modal.classList.add('active')
+    let idCourser = card.getAttribute("id");
 
-        if (modal.querySelector(".modal").classList.contains('maximize')) {
-            modal.querySelector(".modal").classList.remove('maximize')
-        }
-       
-        let idCourser = card.getAttribute("id")
-
-        iframe.setAttribute('src', `https://rocketseat.com.br/${idCourser}`)
-
-    })
+    iframe.setAttribute("src", `https://rocketseat.com.br/${idCourser}`);
+  });
 }
 
-modal.querySelector(".modal .button-close").addEventListener('click', ()=> {
-    modal.classList.remove('active')
-    iframe.setAttribute('src','')
-})
+modal.querySelector(".modal .button-close").addEventListener("click", () => {
+  modal.classList.remove("active");
+  iframe.setAttribute("src", "");
+});
 
-modal.querySelector(".modal .button-maximize").addEventListener('click', ()=> {
-    modal.querySelector(".modal").classList.toggle('maximize')
-})
-
+modal.querySelector(".modal .button-maximize").addEventListener("click", () => {
+  modal.querySelector(".modal").classList.toggle("maximize");
+});
