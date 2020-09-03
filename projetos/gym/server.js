593197192cef381bb44207fd613aded97e5ece0a
Edvan Ribeiro <jk.jr47@gmail.com>
2020-09-03 00:21:58 -0300

    Atualização do projeto

---
+++ projetos/gym/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const nunjucks = require("nunjucks");
+const routes = require("./routes")
+const server = express();
+
+const port = 5000
+
+server.use(express.static("public"));
+server.use(routes)
+
+server.set("view engine", "njk");
+
+nunjucks.configure("views", {
+  express: server,
+  autoescape: false,
+  noCache: true
+});
+
+server.listen(port, function () {
+  console.log("server is running");
+});
